64663cea0cf81b0af8c463d82592ef1c4017aac3
GreatAlex33 <15arhireev15@mail.ru>
2025-04-11 19:56:07 +0300

    am i stupid or study is hard?

--- project1/dirt.py/dirt.py
+++ project1/dirt.py/dirt.py
@@ -1,3 +1,5 @@
 import numpy as np
 arr = np.linspace(-6, 21, num=60,endpoint=False, retstep=True)
 print(arr)
+
+
--- project1/pandas/pan3.py
+++ project1/pandas/pan3.py
@@ -9,11 +9,26 @@ data = data.drop(['birth year'], axis=1)
 #data['starttime'] = pd.to_datetime(data['starttime']).dt.minute
 #data['stoptime'] = pd.to_datetime(data['stoptime']).dt.minute
 #data['trip duration'] = data['stoptime'] - data['starttime']
-data['starttime'] = pd.to_datetime(data['starttime']).dt.day_of_week
-def weekofbike(dat):
-    if dat >= 5:
-        return 1
-    else:
-        return 0
-data['weekend'] = data['starttime'].apply(weekofbike)
-data['weekend'].value_counts()
+#data['starttime'] = pd.to_datetime(data['starttime']).dt.day_of_week
+#def weekofbike(dat):
+#    if dat >= 5:
+#        return 1
+#    else:
+#        return 0
+#data['weekend'] = data['starttime'].apply(weekofbike)
+#data['weekend'].value_counts()
+
+data['time_of_day'] = pd.to_datetime(data['starttime']).dt.hour
+def get_time_of_day(tame):
+    if (6 >= tame and tame >= 0):
+        return 'night'
+    if (7 <= tame and tame <= 12):
+        return 'morning'
+    if (13 <= tame and tame <= 18):
+        return  'day'
+    if 19 <= tame and tame <= 23:
+        return  'evening'
+
+
+data['time_of_day'] = data['time_of_day'].apply(get_time_of_day)
+display((data[data['time_of_day'] == 'day'].shape[0]) / (data[data['time_of_day'] == 'night'].shape[0]))
