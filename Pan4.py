c959d49e7700f482551d9c896370a3acac598f04
GreatAlex33 <15arhireev15@mail.ru>
2025-04-15 19:22:56 +0300

    1

--- Pan4.py
+++ Pan4.py
@@ -4,18 +4,23 @@ import pandas as pd
 from regex import D
 
 melb_data = pd.read_csv('data/melb_data_fe.csv', sep=',')
-display(melb_data)
+
 
 #Преобразуйте столбец Date в формат datetime и выделите квартал (quarter) продажи объектов недвижимости. 
 #Найдите второй по популярности квартал продажи. В качестве ответа запишите число объектов, проданных в этом квартале.
 melb_data['Date'] = pd.to_datetime(melb_data['Date']).dt.quarter
 #melb_data.info()
 
-nuniqcol = melb_data.apply(lambda x: x.nunique() < 150)
-for x in melb_data:
-    foreighn_lst = ['Date', 'Rooms', 'Bedroom', 'Bathroom', 'Car']
-    if nuniqcol[x] == True and x not in foreighn_lst:
-        melb_data[x] = melb_data[x].astype('category')
-    else:
-        pass
-display(melb_data.info())
+#nuniqcol = melb_data.apply(lambda x: x.nunique() < 150)
+#for x in melb_data:
+    #foreighn_lst = ['Date', 'Rooms', 'Bedroom', 'Bathroom', 'Car']
+    #if nuniqcol[x] == True and x not in foreighn_lst:
+       # melb_data[x] = melb_data[x].astype('category')
+    #else:
+        #pass
+
+#melb_data.sort_values(by='AreaRatio', ascending=False, ignore_index=True)
+
+
+
+display(melb_data.groupby('Regionname')['Lattitude'].std().sort_values(ascending=False))
--- project1/dirt.py/dirt.py
+++ project1/dirt.py/dirt.py
@@ -4,3 +4,10 @@ from IPython.display import display
 import pandas as pd
 from regex import D
 
+mask1 = melb_data['Type'] == 'townhouse'
+mask2 = melb_data['Rooms'] > 2
+melb_data[mask1 & mask2].sort_values(
+    by=['Rooms', 'MeanRoomsSquare'],
+    ascending=[True, False],
+    ignore_index=True)
+display(melb_data.loc[18])
