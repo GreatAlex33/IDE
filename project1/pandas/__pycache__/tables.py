1b9854ef598318ade038110ff6635121d3fe56bf
GreatAlex33 <15arhireev15@mail.ru>
2025-04-23 20:29:06 +0300

    upd

---
+++ project1/pandas/__pycache__/tables.py
@@ -0,0 +1,29 @@
+from re import X
+from xml.etree.ElementInclude import include
+from IPython.display import display
+import pandas as pd
+from regex import D
+
+
+covid_data = pd.read_csv('data/covid_data.csv')
+vaccinations_data = pd.read_csv('data/country_vaccinations.csv')
+vaccinations_data = vaccinations_data[
+    ['country', 'date', 'total_vaccinations', 
+     'people_vaccinated', 'people_vaccinated_per_hundred',
+     'people_fully_vaccinated', 'people_fully_vaccinated_per_hundred',
+     'daily_vaccinations', 'vaccines']
+]
+
+covid_data = covid_data.groupby(
+    ['date', 'country'], 
+    as_index=False
+)[['confirmed', 'deaths', 'recovered']].sum()
+covid_data['date'] = pd.to_datetime(covid_data['date'])
+covid_data['active'] = covid_data['confirmed'] - covid_data['deaths'] - covid_data['recovered']
+covid_data = covid_data.sort_values(by=['country', 'date'])
+covid_data['daily_confirmed'] = covid_data.groupby('country')['confirmed'].diff()
+covid_data['daily_deaths'] = covid_data.groupby('country')['deaths'].diff()
+covid_data['daily_recovered'] = covid_data.groupby('country')['recovered'].diff()
+
+vaccinations_data['date'] = pd.to_datetime(vaccinations_data['date'])
+display(vaccinations_data['date'].min())
