0117f6e3e9b0fdb2ea874aeddb1f73f4386e9f97
GreatAlex33 <15arhireev15@mail.ru>
2025-06-15 19:49:40 +0300

    Delete 111.py

--- 111.py
+++
@@ -1,6 +0,0 @@
-import os
-
-print(os.listdir()) # ['SnapchatLoader', 'FBLoader', 'tmp.py', '.gitignore', 'venv', '.git']
-
-if 'tmp.py' not in os.listdir():
-    print("Файл отсутствует в данной директории")
